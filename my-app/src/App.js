11aa185bec99cde8a5b093a6ea1d33fb5dd557f7
Erica Butts <erica@kintone.com>
2022-04-19 20:48:39 -0400

    Weather Card and weather api.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,21 +13,11 @@ import RankingList from "./components/Countries/RankingList";
 import EducationMainPage from "./components/Education/EducationMainPage";
 
 import Footer from "./components/Footer";
-<<<<<<< HEAD
 import { Sample } from "./components/pages/Sample";
 import { Signup } from "./components/pages/Signup";
 import { Login } from "./components/pages/Login";
 import { Forums } from "./components/pages/Forums";
 import ForumTile from "./components/pages/ForumTiles";
-=======
-import DetailedArticle from "./components/Education/DeatiledArticle";
-import EducationRankingList from "./components/Education/EducationRankingList";
-import UniversityRanking from "./components/Education/UniversityRanking";
-import SignIn from "./components/SignIn";
-import ElectionPage from "./components/Election/ElectionMainFile";
-import ElectionDetailed from "./components/Election/ElectionDetailed";
-import EventMainPage from "./components/Events/EventMainPage";
->>>>>>> 8b78d3d1cecaebf362ca1402677998ab098fa395
 
 function App() {
   return (
@@ -37,7 +27,6 @@ function App() {
         <NavBar />
         <br/>
         <Routes>
-<<<<<<< HEAD
           <Route path="/sample" element = {<Sample />} />
           <Route path='/home' element = {<Home />} />
           <Route path='' element = {<Home />} />
@@ -47,29 +36,6 @@ function App() {
           <Route path="/login" element = {<Login />} />
           <Route path="forums" element = {<Forums/>} />
           <Route path="forumtiles" element = {<ForumTile/>} />
-=======
-        {/* <Route path="/signIn" element = {<SignIn />} /> */}
-          <Route path="/" element = {<Home />} />
-          <Route path='home' element = {<Home />} />
-          <Route path="trending" element = {<Trending />} />
-          <Route path="articlebody" element = {<ArticleBody />} />
-          <Route path="newsSection" element = {<FirstPageNews />} />
-          <Route path="countries" element = {<CountryMainPage />} />
-          {/* <Route path="detailedNews" element = {<} /> */}
-          <Route path="detailedNews" element = {<DetailedNews />} />
-          <Route path="rankingList" element = {<RankingList />} />
-          <Route path="education" element = {<EducationMainPage />} />
-          <Route path="detailedArticle" element = {<DetailedArticle />} />
-          <Route path="educationRankingList" element = {<EducationRankingList />} />
-          {/* <Route path="universityRanking" element = {<UniversityRanking />} /> */}
-          <Route path="universityRanking" element = {<UniversityRanking />} />
-          <Route path="election" element = {<ElectionPage />} />
-          <Route path="electionDetailed" element = {<ElectionDetailed />} />
-          <Route path="event" element = {<EventMainPage />} />
-          
-          
-          
->>>>>>> 8b78d3d1cecaebf362ca1402677998ab098fa395
         </Routes>
       </BrowserRouter>
       <Footer />
