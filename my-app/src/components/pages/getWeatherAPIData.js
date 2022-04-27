3e5238a1ee78d403ec96b9b682729e21db11171e
Hollinss <hj84@njit.edu>
2022-04-27 17:50:02 -0400

    improved weather card on Homepage

--- my-app/src/components/pages/getWeatherAPIData.js
+++ my-app/src/components/pages/getWeatherAPIData.js
@@ -1,6 +1,6 @@
-export const getWeatherAPIData = async ()=>{
+export const getWeatherAPIData = async (chosenCity)=>{
     var apiKey = '7aad8de6434841d0afd233500221904';
-    var url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London&aqi=no`
+    var url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${chosenCity}&aqi=no`
     const response = await fetch(url);
     const json = await response.json();
     return json;
